88db05d64fc2c718f3793d927919d079ff311459
Vitali Hauzinski <hauzinski@gmail.com>
2022-02-13 19:30:22 +0300

    refactor: code formatting

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,5 @@
 import '../styles/main.css';
 import StartApp from './components/app/start';
-import StartAudiochallengeApp from './components/app/audiochallenge';
 
 const startApp = new StartApp();
 startApp.render();
