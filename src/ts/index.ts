43dd2621793baead4c1b343bc10475dec52110c1
Vitali Hauzinski <hauzinski@gmail.com>
2022-02-13 12:58:50 +0300

    refactor: change styles

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,16 +2,7 @@ import '../styles/main.css';
 import StartApp from './components/app/start';
 import StartAudiochallengeApp from './components/app/audiochallenge';
 
-// const startApp = new StartApp();
-// startApp.render();
-
-// const startApp = new StartApp();
-// const startAudiochallengeApp = new StartAudiochallengeApp();
-
-// async function test() {
-//   await startApp.render();
-//   await startAudiochallengeApp.startGame();
-//   startAudiochallengeApp.addListeners(); 
-// }
-
-// test();
+const startApp = new StartApp();
+startApp.render();
+const startAudiochallengeApp = new StartAudiochallengeApp();
+startAudiochallengeApp.addListeners(); 
