331be52a887ce0406cf88be34cdd7b35fd06f56f
Vitali Hauzinski <hauzinski@gmail.com>
2022-02-14 10:34:42 +0300

    refactor: code formatting

--- src/ts/components/app/game-difficulty.ts
+++ src/ts/components/app/game-difficulty.ts
@@ -4,15 +4,15 @@ import GameDifficultyButtonsContent from '../view/game-difficulty/level-buttons/
 
 class OpenGameDifficultyPage {
   public async render(header: string, ...text: Array<string>) {
-    const pageContainer = document.querySelector('.page-container') as HTMLElement;
-    const page = document.createElement('div');
-    page.classList.add('game-difficulty-container');
-    page.innerHTML = await GameDifficultyContent.render();
-    pageContainer.append(page);
+    const PageContainer = document.querySelector('.page-container') as HTMLElement;
+    const Page = document.createElement('div');
+    Page.classList.add('game-difficulty-container');
+    Page.innerHTML = await GameDifficultyContent.render();
+    PageContainer.append(Page);
 
-    const container = document.querySelector('.game-difficulty-container__content') as HTMLElement;
-    container.insertAdjacentHTML('beforeend', await GameDifficultyDescriptionContent.render(header, ...text));    
-    container.insertAdjacentHTML('beforeend', await GameDifficultyButtonsContent.render());   
+    const Container = document.querySelector('.game-difficulty-container__content') as HTMLElement;
+    Container.insertAdjacentHTML('beforeend', await GameDifficultyDescriptionContent.render(header, ...text));    
+    Container.insertAdjacentHTML('beforeend', await GameDifficultyButtonsContent.render());   
   }  
 }
 
